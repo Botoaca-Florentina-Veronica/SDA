7c56bb4312f700d0c3785e8ee2c3b54fb083e7d0
Botoaca Florentina Veronica <126331242+Botoaca-Florentina-Veronica@users.noreply.github.com>
2023-10-21 19:03:59 +0300

    Update heapSort.c

--- heapSort.c
+++ heapSort.c
@@ -6,7 +6,7 @@
 void swap(int* a, int* b)
 {
 	int temp;
-    temp = *a;
+        temp = *a;
 	*a = *b;
 	*b = temp;
 }
@@ -14,21 +14,21 @@ void swap(int* a, int* b)
 // To heapify a subtree rooted with node i which is an index in arr[]. N is size of heap
 void heapify(int arr[], int N, int i)
 {
-	// Find largest among root, left child and right child
-	// Initialize largest as root
-	int largest = i;
-	int left = 2*i;
-	int right = 2*i+1;
+    // Find largest among root, left child and right child
+    // Initialize largest as root
+    int largest = i;
+    int left = 2*i;
+    int right = 2*i+1;
 
-	// If left child is larger than root
-	if (left<N && arr[left]>arr[largest])
+    // If left child is larger than root
+    if (left<N && arr[left]>arr[largest])
     {
-		largest = left;
+	largest = left;
     }
-	// If right child is larger than largest so far
-	if (right<N && arr[right]>arr[largest])
+    // If right child is larger than largest so far
+    if (right<N && arr[right]>arr[largest])
     {
-		largest = right;
+	largest = right;
     }
     // Swap and continue heapifying if root is not largest
     // If largest is not root:
@@ -46,12 +46,12 @@ void heapify(int arr[], int N, int i)
 void heapSort(int arr[], int N)
 {
     int i;
-	// Build max heap
-	for (i=N/2; i>=1; i--)
+    // Build max heap
+    for (i=N/2; i>=1; i--)
     {
         //aici practic merg la ultimul parinte al arborelui pentru a aplica heapify, dupa merg tot mai in sus
         //catre varful arborelui, pentru a analiza toti parintii
-		heapify(arr, N, i);
+	heapify(arr, N, i);
     }
 	
     // Heap sort
