dd507ebeb0ab69c42830cd3bf6caea8d376a438c
Botoaca Florentina Veronica <126331242+Botoaca-Florentina-Veronica@users.noreply.github.com>
2023-10-28 22:51:37 +0300

    Update heapSort.c

--- heapSort.c
+++ heapSort.c
@@ -14,7 +14,7 @@ void swap(int* a, int* b)
 void heapify(int arr[], int N, int i)
 {
     //heapify imi face practic doar o operatie, imi verifica copiii unui parinte, iar daca unul e mai mare decat acesta, face swap	
-    int largest = i;
+    int largest = i;  //la min heap voi avea smallest si in if-uri schimb din > in <
     int left = 2*i;
     int right = 2*i+1;
 
@@ -53,6 +53,9 @@ void heapSort(int arr[], int N)
 	swap(&arr[0], &arr[i]);
 	//aici practic fac schimb cu primul element din vector, fiindca folosind (Max)heapify mai sus, in varful arborelui
 	//voi avea cel mai mare numar, deci il pozitionez la final si continui 
+	    
+	// la min heap voi avea practic acelasi cod, doar ca acum informatia aflata in varful arborelui binar este cel mai mic element
+	//deci cand voi sterge elementul din varf si il voi pozitiona la finalul vectorului, in locul liber ramas, voi crea un sir descrescator
 	heapify(arr, i, 0);
     }
 }
