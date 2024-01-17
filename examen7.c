876c5d17aee48775353182e390d37047267d0c9d
Botoaca Florentina Veronica <126331242+Botoaca-Florentina-Veronica@users.noreply.github.com>
2024-01-17 14:44:28 +0200

    Update examen7.c

--- examen7.c
+++ examen7.c
@@ -9,17 +9,6 @@ Este obligatorie comentarea algoritmului si specificarea complexitatii acestuia 
 Este obligatorie exemplificarea efectului apelului functiilor implementate pe un exemplu ales.
 */
 
-/*
-Fie un tablou de structuri de forma struct echipa{char nume[25]; int puncte; int buget, char oras[25]}. Ştiind ca tabloul are dimensiunea N, sa se implementeze cate o funcție pentru:
-a. Mutarea pe primele pozitii din vector a echipelor cu punctaj mai mare decat 45, iar pe ultimele pozitii a celor cu punctaj mai mic decat 20. Algoritmul va avea o complexitate de O(n), si nu va folosi un alt tablou auxiliar;
-b. Sortarea cat mai eficienta a echipelor cu un buget mai mare decat 100 000 (celalalte echipe nu vor fi sortate pastrandu-si aceleasi pozitii in tablou) alfabetica in functie de nume.
-Sa se considere un exemplu de tablou cu minim 5 astfel de echipe și sa se exemplifice cum va arata tabloul dupa apelul fiecareia din functiile de mai sus.
-Nota:
-Algoritmul poate fi scris in pseudocod (C like).
-Este obligatorie comentarea algoritmului si specificarea complexitatii acestuia in termeni de O(f(n)), comentarea structurilor de date alese, precum si justificarea alegerii acestora.
-Este obligatorie exemplificarea efectului apelului functiilor implementate pe un exemplu ales.
-*/
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -48,11 +37,11 @@ void quicksort(echipa echipe[], int prim, int ultim)
 	int pivot = prim;
 	while (stanga <= dreapta) //partitionare
 	{
-		while (strcmp(echipe[stanga].nume , echipe[pivot].nume) < 0)
+		while (stanga >= ultim && strcmp(echipe[stanga].nume, echipe[pivot].nume) < 0)
 		{
 			stanga++;
 		}
-		while (strcmp(echipe[dreapta].nume,  echipe[pivot].nume) > 0)
+		while (dreapta < prim && strcmp(echipe[dreapta].nume, echipe[pivot].nume) > 0)
 		{
 			dreapta--;
 		}
@@ -97,11 +86,11 @@ void mutareEchipe(echipa echipe[], int N)
 	pozUltim = N - 1;
 	while (pozPrim <= pozUltim)
 	{
-		while (echipe[pozPrim].puncte > 45)
+		while (pozPrim < N && echipe[pozPrim].puncte > 45)
 		{
 			pozPrim++;
 		}
-		while (echipe[pozUltim].puncte < 20)
+		while (pozUltim >= 0 &&echipe[pozUltim].puncte < 20)
 		{
 			pozUltim--;
 		}
@@ -115,13 +104,13 @@ void mutareEchipe(echipa echipe[], int N)
 // Functie pentru sortarea echipelor cu buget mai mare decat 100,000 alfabetic dupa nume
 void sortareAlfabetica(echipa echipe[], int N)
 {
-	int i, pozPrim, pozUltim;
+	int  pozPrim, pozUltim;
 	pozPrim = 0;
 	pozUltim = N - 1;
 
 	while (pozPrim <= pozUltim)
 	{
-		while (echipe[pozPrim].buget > 100000)
+		while (pozPrim <N && echipe[pozPrim].buget > 100000)
 		{
 			pozPrim++;
 		}
@@ -135,18 +124,20 @@ void sortareAlfabetica(echipa echipe[], int N)
 		}
 	}
 
-	// Apelam functia de sortare alfabetica pentru echipele cu buget mai mare decat 100,000
-	quicksort(echipe, 0, pozPrim - 1);
+	if (pozUltim >= 0)
+	{  // Verificare pentru a evita accesarea în afara limitelor
+		quicksort(echipe, 0, pozUltim);   // Sortează doar echipele cu buget >100000
+	}
 }
 
 int main(void)
 {
-	echipa echipe[] = { {"fcsb", 35, 15000, "Bucuresti"},
-					   {"upt", 66, 2000, "Timisoara"},
-					   {"cluj", 112, 400000, "Cluj-Napoca"},
-					   {"dinamo", 3, 300000, "Ilfov"},
-					   {"real madrid", 8, 10000, "Severin"} };
-	int N, i;
+	echipa echipe[] = { {"fcsb", 35, 150000, "Bucuresti"},
+					   {"upt", 66, 200, "Timisoara"},
+					   {"cluj", 112, 4000, "Cluj-Napoca"},
+					   {"dinamo", 3, 3000, "Ilfov"},
+					   {"real madrid", 8, 300000, "Severin"} };
+	int N;
 	N = sizeof(echipe) / sizeof(echipe[0]);
 	printf("Lista initiala cu echipe:\n");
 	afiseazaEchipe(echipe, N);
